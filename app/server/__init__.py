8d696e2b512fb75e2ec22d63cf0b38bde690faca
Carlos Espino Timón <carlosespinotimon@gmail.com>
2019-07-09 06:34:29 +0100

    feature/create_initial_structure
    Create initial structure with Dockerfile, docker-compose, Makefile, minimal Flask server and a test

---
+++ app/server/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, jsonify, current_app
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+sys.stdout.flush()
+
+
+def create_app(app_config='config.Dev'):
+    # create and configure the app
+    app = Flask(__name__)
+    app.config.from_object(app_config)
+
+    cors = CORS(app)
+
+
+    # A simple page that says server status
+    @app.route('/')
+    @cross_origin()
+    def home():
+        return jsonify('The server is running!!')
+
+  
+    return app
+
---
+++ app/tests/tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from test_server import *
+'Init'
+import unittest
+import pathmagic
+
+from server import create_app
+
+
+if __name__ == '__main__':
+    unittest.main()
